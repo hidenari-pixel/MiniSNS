48eed527583bc8ae415aff3230a51f509026d876
no-name-pixel <82199171+hidenari-pixel@users.noreply.github.com>
2021-09-03 09:35:48 +0900

    Update useUsersInfomation.ts

--- src/hooks/useUsersInfomation.ts
+++ src/hooks/useUsersInfomation.ts
@@ -8,9 +8,6 @@ const useUsersInfomation = () => {
   const { setUserNames, setUserId } = module.actions;
   const dispatch: AppDispatch = useDispatch();
 
-  // Warning: Can't perform a React state update on an unmounted component.
-  //  This is a no-op, but it indicates a memory leak in your application.
-  //  To fix, cancel all subscriptions and asynchronous tasks in a useEffect cleanup function.
   const getUsers = () => {
     const registeredUsers = [] as string[];
     let newUsers = [] as string[];
